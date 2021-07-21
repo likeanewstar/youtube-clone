196ff50c31797a9faf07f0fc3fbe06602a9243e1
newstar <kangsaebyeol@gmail.com>
2021-07-21 23:44:06 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, [])
   return (
-    <h1>Hello</h1>
+    <VideoList videos={videos} />
   );
 }
 
