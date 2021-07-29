71fe088be352b086b1194b7878c9e2ee0712ddb5
newstar <kangsaebyeol@gmail.com>
2021-07-29 22:44:11 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,32 +3,19 @@ import styles from './app.module.css';
 import Header from './components/header/header'
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos))
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBYZL5rDr4tY5SFwnuUHyiE2GOdZqrnUGM", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos))
   }, [])
   return (
     <div className={styles.app}>
