b2b93f3d5501c92a4758df6b07bc1335d63bfda1
newstar <kangsaebyeol@gmail.com>
2021-07-29 20:50:13 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-    <li>{props.video.snippet.title}</li>            
+const VideoItem = ({video: { snippet }}) => (
+    <li className={styles.videoItem}>
+        <a href="#a">
+            <p className={styles.imgBox}>
+                <img src={snippet.thumbnails.medium.url} alt={snippet.title} />
+            </p>
+            <p className={styles.txtBox}>
+                <span className={styles.title}>{snippet.title}</span>
+                <span className={styles.channelTitle}>{snippet.channelTitle}</span>
+            </p>
+            
+
+        </a>
+    </li>            
 );
 
 export default VideoItem;
